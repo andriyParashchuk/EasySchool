76b1cb0b34ae2b0907acef0c347e8973322353a3
Sergey <sviridov.sv82@gmail.com>
2019-05-18 12:12:24 +0300

    Fix styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
   validates :first_name, :last_name, presence: true, length: { minimum: 2 }
   validates :email,
-    presence: true,
+    presence:   true,
     uniqueness: true,
-    length: { minimum: 5 },
-    format: { with: VALID_EMAIL_REGEX, message: 'Not valid email format' }
+    length:     { minimum: 5 },
+    format:     { with: VALID_EMAIL_REGEX, message: 'Not valid email format' }
 end
